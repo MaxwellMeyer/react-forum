10580fa6ea67341f5fbd12a5385f943894db630c
Maxwell Meyer <maxreadswell@gmail.com>
2021-09-14 14:41:37 -0700

    initial commit, set up component file stucture
    Co-authored-by: mireie <mireie@users.noreply.github.com>

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import PostControl from "./PostControl";
+import Header from "./Header";
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header />
+      <PostControl />
+    </React.Fragment>
+  );
+}
+
+export default App;
