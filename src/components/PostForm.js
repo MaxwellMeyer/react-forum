aa27d4058219186a962f3b0f569fbcedd2c57663
Maxwell Meyer <maxreadswell@gmail.com>
2021-09-15 10:48:46 -0700

    fix delete functionality for posts
    Co-authored-by: mireie <mireie@users.noreply.github.com>

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -4,10 +4,14 @@ import PropTypes from "prop-types";
 function PostForm(props) {
   return (
     <>
+    <div class="row">
+      <div class="col"
       <form onSubmit={props.formSubmissionHandler}>
-        <input type="text" name="title" />
-        <textarea name="body" />
-        <button type="submit"></button>
+        <input className="form-control" type="text" name="title" />
+        <textarea className="form-control" name="body" />
+        <button className="btn btn-primary my-3" type="submit">
+          Add Post
+        </button>
       </form>
     </>
   );
